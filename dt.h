3d6efc8175cadca4a7c7e5c0196034eb9e9fbdb5
anand700 <anand12murthy@gmail.com>
2016-01-16 14:55:28 -0600

    Create dt.h

---
+++ dt.h
@@ -0,0 +1,14 @@
+#ifndef DT_H
+#define DT_H
+
+// define bool if not defined
+#ifndef bool
+    typedef short bool;
+    #define true 1
+    #define false 0
+#endif
+
+#define TRUE true
+#define FALSE false
+
+#endif // DT_H
